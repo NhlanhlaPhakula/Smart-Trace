381522cb7428f07f6f2362ec508096838f86acca
NhlanhlaPhakula <201605802@student.uj.ac.za>
2021-05-12 05:20:32 +0200

    latest updates by Nhlanhla

--- smart-trace-system/src/Pages/Config.js
+++ smart-trace-system/src/Pages/Config.js
@@ -1,8 +1,55 @@
-  import React from 'react';
+  import React, { Component } from 'react';
+  import PropTypes from 'prop-types';
   import { createChatBotMessage } from "react-chatbot-kit";
 
   import LearningOptions from './LearningOptions';
   import LinkList from './LinkList';
+
+  class Review extends Component {
+      constructor(props) {
+          super(props);
+
+          this.state = {
+              name: '',
+              gender: '',
+              age: '',
+          };
+      }
+
+      componentWillMount() {
+          const { steps } = this.props;
+          const { name, gender, age } = steps;
+      }
+
+      render() {
+          const { name, gender, age } = this.state;
+          return (
+              <div style={{ width: '100%' }}>
+                  <h3>Summary</h3>
+                  <table>
+                      <tbody>
+                          <tr>
+                              <td>Name</td>
+                              <td>{name.value}</td>
+                          </tr>
+                          <tr>
+                              <td>Gender</td>
+                              <td>{gender.value}</td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+          );
+      }
+  }
+
+  Review.propTypes = {
+    steps: PropTypes.object,
+  };
+  
+  Review.defaultProps = {
+    steps: undefined,
+  };
   
   const config = {
       botName : "Smart Trace Bot",
@@ -137,10 +184,10 @@
             widgetName: "EnquiryForm",
             widgetFunc: (props) => <LinkList {...props} />,
             props: {
-                steps: [
+                options: [
                     {
                         id: 1,
-                        message: 'What is your name?',
+                        text: 'What is your name?',
                         trigger: 2,
                     },
                     {
@@ -150,7 +197,7 @@
                     },
                     {
                         id: 3,
-                        message: 'Hi {previousValue}! what is your genger?',
+                        text: 'Hi {previousValue}! what is your genger?',
                         trigger: 4,
                     },
                     {
@@ -162,7 +209,7 @@
                     },
                     {
                         id: 5,
-                        message: 'How old are you?',
+                        text: 'How old are you?',
                         trigger: 6,
                     },
                     {
--- smart-trace-system/src/Pages/Report.js
+++ smart-trace-system/src/Pages/Report.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firebase from '../Components/Firebase';
+import Popup from './Popup';
 
 const Report = () => {
+    //variables
+    const [category, setCategory] = useState();
+    const [productName, setProductName] = useState();
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [serialNumber, setSerialNumber] = useState();
+    const [description, setDescription] = useState();
+    //user function
+    const user = firebase.auth().currentUser;
+
+    //popup toggle function
+    const [isOpen, setIsOpen] = useState(false);
+
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    };
+
+    //a function to save data into the database
+    const saveData = () => {
+        const saveRef = firebase.database().ref('Reports');
+
+        const savingData = {
+            userId: user.email,
+            category,
+            productName,
+            firstName,
+            lastName,
+            serialNumber,
+            description,
+        };
+        saveRef.push(savingData);
+    };
+
     return(
-        <div>
+        <div className="report-page">
             <h1>Report Page</h1>
+            <div>
+                {
+                    isOpen && <Popup
+                        content={<>
+                            <b>Smart Trace</b>
+                            <p>You've Successfully Reported a missing item!!</p> 
+                        </>}
+                    handleClose={togglePopup}
+                    />
+                }
+                <label>Product Name</label>
+                <input type="text" value={productName} required onChange={e=> setProductName(e.target.value)} /><br/>
+                <label>Product Type</label>
+                <select value={category} required onChange={e=> setCategory(e.target.value)}>
+                    <option></option>
+                    <option value="Camera">Camera</option>
+                    <option value="Desktop Computer">Desktop Computer</option>
+                    <option value="DvD Player">DvD Player</option>
+                    <option value="Fan">Fan</option>
+                    <option value="Game Console">Game Console</option>
+                    <option value="iPad">iPad</option>
+                    <option value="iPod">iPod</option>
+                    <option value="Laptop">Laptop</option>
+                    <option value="Mobile Phone">Mobile Phone</option>
+                    <option value="Oven">Oven</option>
+                    <option value="Printer">Printer</option>
+                    <option value="Radio">Radio</option>
+                    <option value="Television">Television</option>
+                    <option value="Washing Machine">Washing Machine</option>
+                </select><br/>
+                <label>Owner' FirstName : </label>
+                <input type="text" value={firstName} onChange={e=> setFirstName(e.target.value)} /><br/>
+                <label>Owner' LastName : </label>
+                <input type="text" value={lastName} onChange={e=> setLastName(e.target.value)} /><br/>
+                <label>Product Serial Number :</label>
+                <input type="text" value={serialNumber} onChange={e=> setSerialNumber(e.target.value)} /><br/>
+                <label>Description : </label>
+                <input type="text" value={description} onChange={e=> setDescription(e.target.value)}/><br/>
+                <button onClick={() => {
+                    saveData();
+                    togglePopup();
+                }}>Report</button>
+            </div>
+
         </div>
     );
 };
