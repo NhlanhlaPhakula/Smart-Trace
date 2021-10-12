b97cfb59bdcc5561dc0be44345587a77dc16ef6a
Nhlanhla Phakula <201605802@student.uj.ac.za>
2021-10-12 13:55:27 +0200

    latest updates by Nhlanhla

--- smart-trace-system/src/Pages/AboutUs.js
+++ smart-trace-system/src/Pages/AboutUs.js
@@ -5,24 +5,53 @@ const AboutUs = () => {
         <div className="tab">
             <h1>About Us</h1>
             <div className="divs2">
-                <img src="./images/head.png" />
+            <div className="flex-container">
+                    <div className="first-child">
+                        <img src="./images/lebo.jfif" />
+                    </div>
+                    <div className="information">
+                        <h3>
+                        Lebogang Motimedi<br/>
+                        215052188<br/>
+                        Business analyst<br/>
+                        Tasks: stakeholders requirements , documentation head , presentation along with N Phakula<br/>
+                        UI design of the system along with N Phakula, project team coordinator.<br/>
+                        </h3>
+                    </div>
+                </div>
             </div><br/><br/>
             <div className="divs2">
-                <img src="./images/head.png" />
+            <div className="flex-container">
+                    <div className="first-child">
+                        <img src="./images/jonathan.jpeg" />
+                    </div>
+                    <div className="information">
+                        <h3>
+                        Nhlahla Nhluvuko Phakula<br/>
+                        201605802<br/>
+                        Lead Programmer<br/>
+                        Tasks: Web Development , Mobile Development , presentations, UI design<br/>
+                        Database Management, project team coordinator.<br/>
+                        </h3>
+                    </div>
+                </div>
             </div><br/><br/>
             <div className="divs2">
-                <img src="./images/head.png" />
-            </div><br/><br/>
-            <div className="divs2">
-                <div className="flex-container">
+            <div className="flex-container">
                     <div className="first-child">
                         <img src="./images/head.png" />
                     </div>
-                    <div className="">
-                        <h3>John Snow</h3>
+                    <div className="information">
+                        <h3>
+                        Lebogang Motimedi<br/>
+                        215052188<br/>
+                        Business analyst<br/>
+                        Tasks: stakeholders requirements , documentation head , presentation along with N Phakula<br/>
+                        UI design of the system along with N Phakula, project team coordinator.<br/>
+                        </h3>
                     </div>
                 </div>
-            </div>
+                </div>
            
         </div>
     );
